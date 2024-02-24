19bdbec23594d27afabce2a2195734a25e3bb427
junieguo <151979734+junieguo@users.noreply.github.com>
2024-02-24 15:53:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
+    // Initially hide all language elements except the default language
     $('[lang="es"], [lang="zh"], [lang="en"]').hide();
 
+    // Change event listener for the language dropdown
     $('#lang-switch').change(function () {
         var lang = $(this).val();
+        // Hide all language elements
         $('[lang]').hide();
+        // Show the selected language element
+        $('[lang="' + lang + '"]').show();
+
+        // Update the link based on the selected language
+        var mapPage = "";
         switch (lang) {
-            case 'ch': 
-                $('[lang="ch"]').show();
-                break;
-            case 'en': 
-                $('[lang="en"]').show();
+            case 'en':
+                mapPage = "map.html";
                 break;
             case 'es':
-                $('[lang="es"]').show();
+                mapPage = "esmap.html";
                 break;
             case 'zh':
-                $('[lang="zh"]').show();
+                mapPage = "zhmap.html";
                 break;
             default:
-                $('[lang="ch"]').show();
+                mapPage = "map.html";
         }
+        $('a[href="map.html"]').attr('href', mapPage);
     });
 });
